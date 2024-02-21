201f2fe66b2695f4e44ed69c763f5cc31423e789
Saalu Issaka <saaluissaka@gmail.com>
2024-02-21 17:27:00 +0000

    Home basic feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,36 @@
-'use client'
+import {Poppins} from 'next/font/google';
+
+import {cn} from '@/lib/utils';
 import {Button} from '@/components/ui/button';
+import LoginButton from '@/components/login-button';
+
+const font  = Poppins({
+  subsets: ["latin"],
+  weight: ["600"]
+})
 
 export default function Home() {
 
-  console.log('WHERE IS THE MAIN COMPONENT');
   return (
+    <main className='flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800'>
+      <div className="space-y-6 text-center">
+        <h1 className={cn("text-6xl font-semibold text-white drop-shadow-md",
+        font.className,
+        )}>
+            Auth
+        </h1>
+        <p className="text-white  text-lg">
+          A simple authentication service
+        </p>
 
-<div>
-<p className="font-bold  text-green-500">
-Hello Auth
+      </div>
 
-</p>
-<Button>Click me</Button>
+      <LoginButton>
+        <Button variant='secondary' size='lg'>
+          Sign in
+        </Button>
+      </LoginButton>
 
-</div>
+    </main >
   );
 }
